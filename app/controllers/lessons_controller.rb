e8db82f28999c94bc8bb5a82123c22a40cf70d61
long-hoang <thoanglong@gmail.com>
2019-09-01 08:01:12 +0000

    revert3

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,7 +2,7 @@ class LessonsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
-  skip_before_filter :verify_authenticity_token 
+  skip_before_action :verify_authenticity_token 
 
   def show
   end
