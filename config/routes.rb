5bbc577059d2909fc33aa7b8ad77ff3830fa97e0
long-hoang <thoanglong@gmail.com>
2019-09-01 07:44:52 +0000

    fix authentication problems

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,8 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
-    resources :sections, only: [:update]
-    resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :lessons, only: [:update] 
+    resources :sections, only: [:update] do
       resources :lessons, only: [:create]
     end
     resources :courses, only: [:new, :create, :show] do
